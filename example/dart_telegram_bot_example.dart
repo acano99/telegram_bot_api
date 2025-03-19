9252011116044de3bd074aeae810fef7b74e861c
acano <abrahan.jcano99@gmail.com>
2025-03-19 03:22:06 -0400

    agregado nuevo ejemplo

--- example/dart_telegram_bot_example.dart
+++ example/dart_telegram_bot_example.dart
@@ -1,7 +1,8 @@
 import 'package:dart_telegram_bot/dart_telegram_bot.dart';
+import 'package:path/path.dart';
 
 void main() async {
-  final String token = '7801828955:AAGrkCZ1VFxBnhlv5dy6ULB2od9r1NOArBA';
+  final String token = '7166276410:AAFB8XogBcbfT1E5605Rzlo86QSKUzhAtrM';
   final bot = DartTelegramBot(token: token);
 
   bot.command('start', (context) async {
@@ -10,6 +11,23 @@ void main() async {
 
   bot.command('hello', (context) async {
     await context.reply('Hello World!');
+  });
+
+  bot.command('echo', (context) async {
+    final messageText = context.message?.text;
+    if (messageText != null) {
+      final parts = messageText.split(' ');
+      if (parts.length > 1) {
+        final textToEcho = parts.sublist(1).join(' ').toUpperCase();
+        await context.reply(textToEcho);
+      } else {
+        await context.reply('Debes escribir algo despues de /echo');
+      }
+    }
+  });
+
+  bot.command('sumar', (context) async {
+    context.reply("2+2 = ${2 + 2}");
   });
 
   await bot.start();
